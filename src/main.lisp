95e484e900ee8cb773065e2faa1d0e583122060e
Steve Losh <steve@stevelosh.com>
2017-10-15 18:56:17 -0400

    Add a simple game loop

--- src/main.lisp
+++ src/main.lisp
@@ -6,9 +6,12 @@
 (defparameter *screen-width* 64)
 (defparameter *screen-height* 48)
 (defparameter *cell-size* 16)
-
 (defparameter *assets-directory*
   (merge-pathnames #p"assets/" (deploy:data-directory)))
+
+(defvar *player* nil)
+
+(defvar *inputs* (make-hash-table))
 
 
 ;;;; Config -------------------------------------------------------------------
@@ -37,24 +40,99 @@
   (blt:set "window.cellsize = ~Dx~:*~D" (floor *cell-size* 2))
   (blt:set "window.resizeable = false")
   (blt:set "output.vsync = true")
-  (blt:set "input.filter = keyboard")
+  (blt:set "input.filter = keyboard+")
   (config-fonts))
 
 
+;;;; Aspects ------------------------------------------------------------------
+(define-aspect loc
+  (x :initform 0.0 :type single-float)
+  (y :initform 0.0 :type single-float))
+
+(define-aspect renderable
+  (glyph :initform #\? :type character)
+  (color :initform (blt:white)))
+
+
+;;;; Player -------------------------------------------------------------------
+(define-entity player (loc renderable))
+
+(defun make-player ()
+  (create-entity 'player
+    :renderable/glyph #\@))
+
+(defun tick-player (player)
+  (when (gethash :up *inputs*) (decf (loc/y player)))
+  (when (gethash :down *inputs*) (incf (loc/y player)))
+  (when (gethash :left *inputs*) (decf (loc/x player)))
+  (when (gethash :right *inputs*) (incf (loc/x player))))
+
+
+;;;; Game Logic ---------------------------------------------------------------
+(defun tick ()
+  (tick-player *player*))
+
+
 ;;;; UI -----------------------------------------------------------------------
+(defun blit-player (player)
+  (setf
+    (blt:color) (renderable/color player)
+    (blt:cell-char (* 2 (truncate (loc/x player)))
+                   (* 2 (truncate (loc/y player))))
+    (renderable/glyph player)))
+
 (defun blit ()
   (blt:clear)
-  (blt:print 0 0 "[font=text]Hello!")
+  (blit-player *player*)
   (blt:refresh))
 
+
+;;;; Input --------------------------------------------------------------------
+(defun event ()
+  (if (blt:has-input-p)
+    (blt:key-case (blt:read)
+      ((:w :down) '(:keydown :up))
+      ((:a :down) '(:keydown :left))
+      ((:s :down) '(:keydown :down))
+      ((:d :down) '(:keydown :right))
+      ((:w :up) '(:keyup :up))
+      ((:a :up) '(:keyup :left))
+      ((:s :up) '(:keyup :down))
+      ((:d :up) '(:keyup :right))
+      (:escape '(:quit))
+      (:close '(:quit)))
+    :done))
+
+(defun handle-event (event)
+  (ecase (first event)
+    (:quit (setf *running* nil))
+    (:keydown (setf (gethash (second event) *inputs*) t))
+    (:keyup (setf (gethash (second event) *inputs*) nil))))
+
+(defun handle-events ()
+  (iterate
+    (for event = (event))
+    (until (eql event :done))
+    (when event
+      (handle-event event))))
+
+
 ;;;; Main ---------------------------------------------------------------------
+(defun initialize ()
+  (clrhash *inputs*)
+  (clear-entities)
+  (setf *running* t
+        *player* (make-player)))
+
 (defun run ()
   (blt:with-terminal
-    (setf (blt:color) (blt:white)
-          *running* t)
     (config)
-    (blit)
-    (sleep 10)))
+    (initialize)
+    (iterate
+      (while *running*)
+      (blit)
+      (handle-events)
+      (tick))))
 
 
 ;;;; Entry --------------------------------------------------------------------
@@ -76,3 +154,6 @@
       (run)))
   (sb-ext:exit :code 0))
 
+
+;;;; Scratch ------------------------------------------------------------------
+;; (setf *running* nil)
